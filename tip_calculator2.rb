e68ff75a57d2c2f5662eb8e743bf3c51a57950d4
Sunny Mittal <86.s.mittal@gmail.com>
2014-04-22 04:58:25 +0000

    Add revised tip calculator

---
+++ tip_calculator2.rb
@@ -0,0 +1,16 @@
+print "How much does your meal cost before tax? $"
+meal = Float(gets.chomp)
+print "What is your local tax rate? $"
+tax = Float(gets.chomp)
+print "What percentage would you like to tip? $" 
+tip = Float(gets.chomp)
+
+tax_value = meal * tax / 100
+meal_with_tax = meal + tax_value
+tip_value = meal * tip / 100
+total_cost = meal_with_tax + tip_value
+
+puts "The pre-tax cost of your meal was $#{sprintf('%.2f', meal)}."
+puts "At #{sprintf('%d', tax)}%, tax for this meal is $#{sprintf('%.2f', meal_with_tax)}."
+puts "For a #{sprintf('%d', tip)}% tip, you should leave $#{sprintf('%.2f', tip_value)}."
+puts "The grand total for this meal is then $#{sprintf('%.2f', total_cost)}."
